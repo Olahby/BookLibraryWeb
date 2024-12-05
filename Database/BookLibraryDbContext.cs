31f9988a20e02c90ba536d766d9e5d871f91d64c
Haidara Yushau <tunde.haidara@gmail.com>
2024-12-05 18:17:54 +0100

    category file create

---
+++ Database/BookLibraryDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BookLibraryWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookLibraryWeb.Database
+{
+    public class BookLibraryDbContext: DbContext
+    {
+        public BookLibraryDbContext(DbContextOptions<BookLibraryDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
+using BookLibraryWeb.Database;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<BookLibraryDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
